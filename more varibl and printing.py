ea7bb410132d29b78dfb9333d22651bdc03658d9
Ruchi3007 <ruchi1735@gmail.com>
2019-05-29 07:37:11 -0700

    1st commit

---
+++ more varibl and printing.py
@@ -0,0 +1,15 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 #not a lie
+my_height = 75 #inches
+my_weight = 180 #lbs
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+print(f"Let's talk about {my_name}.")
+print(f"he's {my_height} inches tall.")
+print(f"he's {my_weight} pounds heavy.")
+print(f" actually that's not too heavy.")
+print(f"he's got {my_eyes}eyes  and {my_hair} hair.")
+print(f"his teeth are ususally {my_teeth} depending on the coffee.")
+total = my_age + my_height + my_weight
+print(f" If I add {my_age}, {my_height}, and {my_weight} i get {total}.")
