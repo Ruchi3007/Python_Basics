78bdaac78d2651505d0fccac24bf3bb2a9cce14c
Ruchi3007 <ruchi1735@gmail.com>
2019-05-29 20:26:22 -0700

    2nd commit

--- more varibl and printing.py
+++ more varibl and printing.py
@@ -13,3 +13,15 @@ print(f"he's got {my_eyes}eyes  and {my_hair} hair.")
 print(f"his teeth are ususally {my_teeth} depending on the coffee.")
 total = my_age + my_height + my_weight
 print(f" If I add {my_age}, {my_height}, and {my_weight} i get {total}.")
+
+
+types_of_people = 10
+x = f"There are {types_of_people} types of people"
+
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}"
+print(x)
+print(y)
+print(f"I said : {x}")
+print(f"I also said : {y}")
