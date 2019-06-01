04ba879884dfdf634a1e40be19b99c0b9af3985b
Ruchi3007 <ruchi1735@gmail.com>
2019-06-01 08:33:41 -0700

    second commit

--- numbers and maths.py
+++ numbers and maths.py
@@ -6,3 +6,64 @@ print("   /|")
 print("  / |")
 print(" /  |")
 print("/___|")
+
+print("\nworking with numbers")
+print(2.9867)
+
+print(-2.9867)
+
+print(3 + 4.5)
+
+print(3 * 4 + 5)
+
+
+
+print(3 * (4 + 5))
+
+
+print(10 % 3)
+
+my_num = 5
+print(my_num)
+
+
+my_num = 5
+print(str(my_num) + " my favourite number")
+
+
+
+my_num = -5
+print(abs(my_num))
+
+
+my_num = -5
+print(pow(3,2))
+
+
+
+my_num = -5
+print(max(4,6))
+
+
+
+my_num = -5
+print(min(8,5))
+
+
+
+print(round(3.7))
+
+
+
+from math import *
+
+print(floor(3.7))
+
+
+
+print(ceil(5.7))
+
+
+
+
+print(sqrt(36))
