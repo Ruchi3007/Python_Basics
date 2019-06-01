abd71388abd2c5b3a3d5c784bf167e90366734a7
Ruchi3007 <ruchi1735@gmail.com>
2019-05-31 21:46:34 -0700

    1st commit

--- basic_program.py
+++ basic_program.py
@@ -1,1 +1,11 @@
-print("Mary had a little lamb")
+formatter= "{} {} {} {}"
+print(formatter.format(1,2,3,4))
+print(formatter.format("one", "two","three","four"))
+print(formatter.format(True,False,False,True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "Try your",
+    "Own text here",
+    "Maybe a poem",
+    "Or a song about fear"
+))
